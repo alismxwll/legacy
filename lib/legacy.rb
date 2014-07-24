32829504c1c932c58707eb1721bb10fefec61c18
Ruben Rhodes & Patrick David <student@epicodus.com>
2014-07-24 16:56:39 -0700

    this update gets you nowhere closer to the solution

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -1,3 +1,12 @@
 def legacy(hash)
-
+  hash[1].each do |val|
+  y = 1
+  masterHash = {}
+  newHash = Hash.new{|val,key| val[key] = 1}
+  puts newHash
+  masterHash.merge!(newHash)
+  end
 end
+
+# puts legacy(1,"A")
+
--- spec/legacy_spec.rb
+++ spec/legacy_spec.rb
@@ -3,6 +3,10 @@ require('legacy')
 
 describe('legacy') do
   it('move value A to become a key and the key 1 to become the value') do
-    expect(legacy({1 => "A"})).to(eq({"A" => 1}))
+    expect(legacy({1=>["A"]})).to(eq({"A"=>1}))
+  end
+
+  it('move value A to become a key and the key 1 to become the value') do
+    expect(legacy({1  => ["A", "E", "I"]})).to(eq({"A"=>1, "E"=>1,"I"=>1}))
   end
 end
