e887477f83b9b430c6204a84e359a44a933d3579
Andrew Akers & Patrick David <student@epicodus.com>
2014-07-25 11:23:48 -0700

    refactors with the complete original hash

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -1,52 +1,9 @@
 def legacy(hash)
   new_hash = Hash.new
-  hash.each do |key, value|
-    value.each do |index|
-      number = key
-      new_hash[index] = number
+  hash.each do |key, value|#loops through the legacy hash
+    value.each do |index|#loops through the array
+      new_hash[index] = key
     end
   end
-  puts new_hash
   new_hash
 end
-  # y = 1
-  # masterHash = {}
-  # newHash = Hash.new{|val,key| val[key] = 1}
-  # puts newHash
-  # masterHash.merge!(newHash)
-
-#   values = hash.values
-#   keys = hash.keys
-
-#   h = values[0],keys[0]
-#   # h.keys
-#   # puts h.keys[0]
-#   # h.keys[0] = h.values[0].to_s
-#   # puts h.keys[0]
-#   new_hash = hash.invert
-#   new_hash.keys
-#   puts new_hash.keys[0]
-#   puts new_hash
-# end
-
-
-# # legacy({1=>["A"]})
-
-
-
-
-
-
-
-
-
-
-
-
-
-# legacy hash
-# keys_array =pull keys out
-# value_array = pull values
-# new_hash <<values in as keys
-# new_hash << keys in as values
-
--- spec/legacy_spec.rb
+++ spec/legacy_spec.rb
@@ -5,10 +5,26 @@ describe('legacy') do
   it('move value A to become a key and the key 1 to become the value') do
     expect(legacy({1=>["A"]})).to(eq({"A"=>1}))
   end
-  it('move value A to become a key and the key 1 to become the value') do
+  it('move values to become keys and the key  to become the value') do
     expect(legacy({1  => ["A", "E", "I"]})).to(eq({"A"=>1, "E"=>1,"I"=>1}))
   end
-  it('move value A to become a key and the key 1 to become the value') do
+  it('move values to become a key and there are multiple keys that will become its appropriate value') do
     expect(legacy({1  => ["A", "E", "I"],  2  => ["D", "G"]})).to(eq({"A"=>1, "E"=>1,"I"=>1, "D" => 2, "G" => 2}))
+  end
+  it('takes the original legacy hash and converts into updated hash') do
+    expect(legacy({ 1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2  => ["D", "G"],
+  3  => ["B", "C", "M", "P"],
+  4  => ["F", "H", "V", "W", "Y"],
+  5  => ["K"],
+  8  => ["J", "X"],
+  10 => ["Q", "Z"]
+})).to(eq({ "A" => 1, "B" => 3,  "C" => 3, "D" => 2, "E" => 1,
+  "F" => 4, "G" => 2,  "H" => 4, "I" => 1, "J" => 8,
+  "K" => 5, "L" => 1,  "M" => 3, "N" => 1, "O" => 1,
+  "P" => 3, "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+  "U" => 1, "V" => 4,  "W" => 4, "X" => 8, "Y" => 4,
+  "Z" => 10
+}))
   end
 end
