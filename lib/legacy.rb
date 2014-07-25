d203856ae150013761b8e0dbf5cbc2cc9b67cfb2
Andrew Akers & Patrick David <student@epicodus.com>
2014-07-25 11:11:29 -0700

    you can now go through the hash and the array items will now be keys and its value will be the originial asscoiated key

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -1,12 +1,52 @@
 def legacy(hash)
-  hash[1].each do |val|
-  y = 1
-  masterHash = {}
-  newHash = Hash.new{|val,key| val[key] = 1}
-  puts newHash
-  masterHash.merge!(newHash)
+  new_hash = Hash.new
+  hash.each do |key, value|
+    value.each do |index|
+      number = key
+      new_hash[index] = number
+    end
   end
+  puts new_hash
+  new_hash
 end
+  # y = 1
+  # masterHash = {}
+  # newHash = Hash.new{|val,key| val[key] = 1}
+  # puts newHash
+  # masterHash.merge!(newHash)
 
-# puts legacy(1,"A")
+#   values = hash.values
+#   keys = hash.keys
+
+#   h = values[0],keys[0]
+#   # h.keys
+#   # puts h.keys[0]
+#   # h.keys[0] = h.values[0].to_s
+#   # puts h.keys[0]
+#   new_hash = hash.invert
+#   new_hash.keys
+#   puts new_hash.keys[0]
+#   puts new_hash
+# end
+
+
+# # legacy({1=>["A"]})
+
+
+
+
+
+
+
+
+
+
+
+
+
+# legacy hash
+# keys_array =pull keys out
+# value_array = pull values
+# new_hash <<values in as keys
+# new_hash << keys in as values
 
--- spec/legacy_spec.rb
+++ spec/legacy_spec.rb
@@ -5,8 +5,10 @@ describe('legacy') do
   it('move value A to become a key and the key 1 to become the value') do
     expect(legacy({1=>["A"]})).to(eq({"A"=>1}))
   end
-
   it('move value A to become a key and the key 1 to become the value') do
     expect(legacy({1  => ["A", "E", "I"]})).to(eq({"A"=>1, "E"=>1,"I"=>1}))
+  end
+  it('move value A to become a key and the key 1 to become the value') do
+    expect(legacy({1  => ["A", "E", "I"],  2  => ["D", "G"]})).to(eq({"A"=>1, "E"=>1,"I"=>1, "D" => 2, "G" => 2}))
   end
 end
